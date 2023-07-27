6d0a4bc7779b5c2c758ab0d8297ec5524324d79f
Philipp <robeck-p@email.ulster.ac.uk>
2023-07-28 00:16:28 +0200

    typo

--- RUH/NDVI-auto-processing/main.py
+++ RUH/NDVI-auto-processing/main.py
@@ -203,8 +203,8 @@ def add_NDVI(image):
 
     # ndvi02 = ndvi.gte(0.2)
     thres = ndvi.gte(0.2).rename('thres')
-    ndvi = ndvi.addBands(areaPixel)
-    ndviImg = image.addBands(ndvi).updateMask(thres)
+    # ndviImg = ndviImg.addBands(areaPixel)
+    ndviImg = image.addBands(areaPixel).addBands(ndvi).updateMask(thres)
 
     maskedPixelCount = ndviImg.select('areaPixel').reduceRegion(
         reducer=ee.Reducer.sum(),
